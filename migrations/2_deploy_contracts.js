ff79a2a0c538d368bd47623da1a2c22362be423a
Roman Strilets <roman@beam-mw.com>
2021-01-27 23:45:01 +0300

    implemented migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,3 +1,7 @@
+var erc20_swap_contract_aggregate_signature = artifacts.require("../contracts/erc20_swap_contract_aggregate_signature.sol");
+var swap_contract_aggregate_signature = artifacts.require("../contracts/swap_contract_aggregate_signature.sol");
+
 module.exports = function(_deployer) {
-  // Use deployer to state migration tasks.
+  _deployer.deploy(erc20_swap_contract_aggregate_signature);
+  _deployer.deploy(swap_contract_aggregate_signature);
 };
