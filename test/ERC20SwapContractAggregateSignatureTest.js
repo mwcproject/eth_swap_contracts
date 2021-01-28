eafb1a11c6aa3683dbd592f47a02c23f317815c6
Roman Strilets <roman@beam-mw.com>
2021-01-29 01:22:20 +0300

    "should initiate" test

--- test/ERC20SwapContractAggregateSignatureTest.js
+++ test/ERC20SwapContractAggregateSignatureTest.js
@@ -1,12 +1,33 @@
 const ERC20SwapContractAggregateSignature = artifacts.require('../contracts/ERC20SwapContractAggregateSignature.sol');
 
 contract('ERC20SwapContractAggregateSignature', function (accounts) {
-    let contract
-  
-    before(async () => {
-        contract = await ERC20SwapContractAggregateSignature.new()
+    let contract;
+
+    beforeEach(async () => {
+        contract = await ERC20SwapContractAggregateSignature.new();
     });
 
-    it('should initiate properly', async () => {
+    it('should initiate', async () => {
+        // let addressFromSecret = '0x1000000000000000000000000000000000000001';
+        // let participant = '0x2000000000000000000000000000000000000002';
+        // let contractAddress = '0x4000000000000000000000000000000000000004';
+        // let sender = accounts[0];
+        // let value = 500;
+        // let refundTimeInBlocks = 333;
+
+        // let result = await contract.getSwapDetails(addressFromSecret);
+
+        // assert.equal(result.initiator, '0x0000000000000000000000000000000000000000');
+        // assert.equal(result.participant, '0x0000000000000000000000000000000000000000');
+
+        // await contract.initiate(refundTimeInBlocks, addressFromSecret, participant, contractAddress, value, {from: sender, value: 0});
+
+        // result = await contract.getSwapDetails(addressFromSecret);
+
+        // assert.equal(result.initiator, sender);
+        // assert.equal(result.participant, participant);
+        // assert.equal(result.contractAddress, contractAddress);
+        // assert.deepEqual(BigInt(result.value), BigInt(value));
+        // assert.deepEqual(BigInt(result.refundTimeInBlocks), BigInt(refundTimeInBlocks));
     });
 });
--- test/SwapContractAggregateSignatureTest.js
+++ test/SwapContractAggregateSignatureTest.js
@@ -1,12 +1,35 @@
 const SwapContractAggregateSignature = artifacts.require('../contracts/SwapContractAggregateSignature.sol');
 
 contract('SwapContractAggregateSignature', function (accounts) {
-    let contract
-  
-    before(async () => {
-        contract = await SwapContractAggregateSignature.new()
+    let contract;
+
+    beforeEach(async () => {
+        contract = await SwapContractAggregateSignature.new();
     });
 
-    it('should initiate properly', async () => {
+    it('should initiate', async () => {
+        let addressFromSecret = '0x1000000000000000000000000000000000000001';
+        let participant = '0x2000000000000000000000000000000000000002';
+        let sender = accounts[0];
+        let value = 500;
+        let refundTimeInBlocks = 333;
+
+        let result = await contract.getSwapDetails(addressFromSecret);
+        let contractBalance = await web3.eth.getBalance(contract.address);
+
+        assert.equal(result.initiator, '0x0000000000000000000000000000000000000000');
+        assert.equal(result.participant, '0x0000000000000000000000000000000000000000');
+        assert.equal(contractBalance, 0);
+
+        await contract.initiate(refundTimeInBlocks, addressFromSecret, participant, {from: sender, value: value});
+
+        result = await contract.getSwapDetails(addressFromSecret);
+        contractBalance = await web3.eth.getBalance(contract.address);
+
+        assert.equal(result.initiator, sender);
+        assert.equal(result.participant, participant);
+        assert.deepEqual(BigInt(result.value), BigInt(value));
+        assert.deepEqual(BigInt(result.refundTimeInBlocks), BigInt(refundTimeInBlocks));
+        assert.equal(contractBalance, value);
     });
 });
