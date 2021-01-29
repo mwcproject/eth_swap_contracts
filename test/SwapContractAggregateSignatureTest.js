8975c0f14855cb196cfed38cb0cf3ef3cd193e1b
Roman Strilets <roman@beam-mw.com>
2021-01-29 11:31:35 +0300

    added 'should not redeem non-exist swap' test

--- test/SwapContractAggregateSignatureTest.js
+++ test/SwapContractAggregateSignatureTest.js
@@ -50,6 +50,22 @@ contract('SwapContractAggregateSignature', function (accounts) {
         }
     });
 
+    it('should not redeem non-exist swap', async () => {
+        let addressFromSecret = '0x4fd44e9919635435cb60e8d0aa0a26cc016f2b99';
+        let participant = '0xa6a2c2fea95702877b9b9a1246bd6d7e0cb7c9e8';
+        let r = '0x8c2e2c85e6adfd38a300c3af38cb6989cdfe846798244e1cb4e2698e12a46316';
+        let s = '0x173e2180fab2fc6dfec81ecd4638fe077073b31ee70729c1fafd6260fad76618';
+        let v = 27;
+
+        try
+        {
+            await contract.redeem(addressFromSecret, r, s, v, {from: participant, value: 0});
+        }
+        catch (error) {
+            assert(error.message.indexOf('invalid msg.sender') >= 0);
+        }
+    });
+
     it('should redeem', async () => {
         // let addressFromSecret = '0x4fd44e9919635435cb60e8d0aa0a26cc016f2b99';
         // let participant = '0xa6a2c2fea95702877b9b9a1246bd6d7e0cb7c9e8';
@@ -64,6 +80,18 @@ contract('SwapContractAggregateSignature', function (accounts) {
         // let v = 27;
 
         // await contract.redeem(addressFromSecret, r, s, v, {from: participant, value: 0});
+    });
+
+    it('should not refund non-exist swap', async () => { 
+        let addressFromSecret = '0x4fd44e9919635435cb60e8d0aa0a26cc016f2b99';
+
+        try
+        {
+            await contract.refund(addressFromSecret, {from: accounts[0], value: 0});
+        }
+        catch (error) {
+            assert(error.message.indexOf('invalid msg.sender') >= 0);
+        }       
     });
 
     it('should refund', async () => {
