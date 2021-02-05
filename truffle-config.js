515e91b2a8a8c8403c32b169e715b42f8c32099d
Roman Strilets <roman@beam-mw.com>
2021-02-05 13:10:22 +0300

    small refactoring

--- truffle-config.js
+++ truffle-config.js
@@ -51,10 +51,10 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-       version: "0.7.0",    // Fetch exact version from solc-bin (default: truffle's version)
+       version: "0.7.6",    // Fetch exact version from solc-bin (default: truffle's version)
        settings: {          // See the solidity docs for advice about optimization and evmVersion
         optimizer: {
-          enabled: false,
+          enabled: true,
           runs: 200
         },
         evmVersion: "byzantium"
